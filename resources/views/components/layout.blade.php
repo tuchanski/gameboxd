db92b9994bc788faff6381b9cc30807abc75ec9d
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-18 14:21:11 -0300

    feat: add base layout & adjust project files permissions

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Teste</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="flex flex-col items-center text-white bg-background min-h-screen">
+
+</body>
+</html>
