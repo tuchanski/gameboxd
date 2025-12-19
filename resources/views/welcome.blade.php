4b7f2eeac8cd451a7a02ed0811ca8f8bb389f5b7
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-19 09:10:58 -0300

    update: post component margin top

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,34 +4,27 @@
 
             <x-menu />
 
-            <div class="mb-[30px]">
-                <x-post
-                    href="#"
-                    title="The Legend of Zelda: Breath of the Wild"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-                    image="https://placehold.co/90x90"
-                    rating="9/10"
-                />
-            </div>
-
-            <div class="mb-[30px]">
-                <x-post
-                    href="#"
-                    title="The Legend of Zelda: Breath of the Wild"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-                    image="https://placehold.co/90x90"
-                    rating="9/10"
-                />
-            </div>
-            <div class="mb-[30px]">
-                <x-post
-                    href="#"
-                    title="The Legend of Zelda: Breath of the Wild"
-                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-                    image="https://placehold.co/90x90"
-                    rating="9/10"
-                />
-            </div>
+            <x-post
+                href="#"
+                title="The Legend of Zelda: Breath of the Wild"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+                image="https://placehold.co/90x90"
+                rating="9/10"
+            />
+            <x-post
+                href="#"
+                title="The Legend of Zelda: Breath of the Wild"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+                image="https://placehold.co/90x90"
+                rating="9/10"
+            />
+            <x-post
+                href="#"
+                title="The Legend of Zelda: Breath of the Wild"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+                image="https://placehold.co/90x90"
+                rating="9/10"
+            />
 
         </div>
     </main>
