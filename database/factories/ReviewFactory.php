160863e76d60c7ef4779f65a5ecb8d573a6f5351
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-19 14:11:39 -0300

    feat: add review factory

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'body' => $this->faker->realText(),
+            'rating' => $this->faker->numberBetween(0,10),
+            'user_id' => User::factory(),
+            'game_id' => fake()->numberBetween(1,5000),
+        ];
+    }
+}
