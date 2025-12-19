7e30c76722b2e59d0b983f135c4ac8dc5c8ddd11
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-19 13:37:14 -0300

    feat: add dots icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Gameboxd</title>
+    <link rel="shortcut icon" type="imagex/svg" href="{{asset('/images/dots-logo.svg')}}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
