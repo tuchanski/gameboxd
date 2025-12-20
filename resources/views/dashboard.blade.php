a7de6c4965137119fa23035b85922ead001debd9
Tuchanski <gui.rocha2004@gmail.com>
2025-12-20 16:29:20 -0300

    update: remove dashboard review hard coded placeholder

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,13 +11,5 @@
         />
     @endforeach
 
-    <x-post
-        href="#"
-        title="The Legend of Zelda: Breath of the Wild"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-        image="https://placehold.co/90x90"
-        rating="9/10"
-    />
-
 </x-layout>
 
