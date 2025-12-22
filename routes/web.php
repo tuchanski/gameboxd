38a2556e0b31202caa2abecd7b90b10b836ee751
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-22 13:38:57 -0300

    cleanup: web routes

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,12 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('dashboard');
-//})->middleware(['auth']);
-
 // Dashboard
-Route::get('/', [ReviewController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/', [ReviewController::class, 'index'])
+    ->name('home')
+    ->middleware('auth');
 
-// Reviews (Posts)
-// Reviews (Posts)
+// Reviews / Posts
 Route::get('/reviews/create', [ReviewController::class, 'create'])
     ->name('review.create')
     ->middleware('auth');
@@ -39,16 +36,31 @@ Route::post('/reviews', [ReviewController::class, 'store'])
     ->name('review.store')
     ->middleware('auth');
 
-
 // Auth
-Route::get('/register', [RegisteredUserController::class, 'create'])->name('register')->middleware('guest');
-Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store')->middleware('guest');
-Route::get("/login", [SessionController::class, 'create'])->name('login')->middleware('guest');
-Route::post("/login", [SessionController::class, 'store'])->name('login.store')->middleware('guest');
-Route::delete("/login", [SessionController::class, 'destroy'])->middleware('auth');
+Route::get('/register', [RegisteredUserController::class, 'create'])
+    ->name('register')
+    ->middleware('guest');
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->name('register.store')
+    ->middleware('guest');
+
+Route::get("/login", [SessionController::class, 'create'])
+    ->name('login')
+    ->middleware('guest');
+
+Route::post("/login", [SessionController::class, 'store'])
+    ->name('login.store')
+    ->middleware('guest');
+
+Route::delete("/login", [SessionController::class, 'destroy'])
+    ->name('login.destroy')
+    ->middleware('auth');
 
 // Gamebrain API
-Route::get('/games', [GamebrainApiController::class, 'index'])->name('games')->middleware('auth');
+Route::get('/games', [GamebrainApiController::class, 'index'])
+    ->name('games')
+    ->middleware('auth');
 
 // About
 Route::get("/about", function () {
