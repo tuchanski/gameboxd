e8a519ae51b701d5fcd9d04f85b2c667d6754a4a
Guilherme Tuchanski Rocha <guilherme.trocha@pucpr.br>
2025-12-22 11:10:03 -0300

    update: correct info in full post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,9 +2,9 @@
     <div class="min-h-[calc(100vh-248px)] w-full flex items-center justify-center">
 
     <x-post.full-post
-            title="The Legend of Zelda: Breath of the Wild"
+            title="{{$review->title}}"
             description="{{$review->body}}"
-            image="https://placehold.co/90x90"
+            image="{{$review->image}}"
             rating="{{$review->rating}}"
         />
     </div>
